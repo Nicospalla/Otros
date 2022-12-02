c260774cab17f9b0131fe0b3bb4f86101c183cca
Nicospalla <nicolasaspalla@gmail.com>
2022-12-02 09:16:13 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String[] nombres = {"Juan", "es", "alto", "gato"};
+
+    for (String nombre : nombres) {
+        System.out.print(nombre + " ");
+    }
+
+    }
+}
